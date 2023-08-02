af87d5a9fd47fe6502c8e2ca68789a308e03ee6f
eguniti <nitin.u.gupta@gmail.com>
2023-08-03 02:08:07 +0530

    vpc modified

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,13 +1,15 @@
 data "aws_availability_zones" "azs" {}
 module "jenkins-vpc" {
   source          = "terraform-aws-modules/vpc/aws"
-  version         = "3.19.0"
+  version         = "5.0.0"
   name            = "jenkins-vpc"
   cidr            = var.vpc_cidr_block
   private_subnets = var.private_subnet_cidr_blocks
   public_subnets  = var.public_subnet_cidr_blocks
   azs             = data.aws_availability_zones.azs.names
 
+  single_nat_gateway   = true
+  enable_nat_gateway   = true
   enable_dns_hostnames = true
 
   tags = {
